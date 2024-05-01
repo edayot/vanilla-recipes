9577bfb03d9754e67a4fe4cdc8edb13cd658048c
edayot <pro.e.dayot@gmail.com>
2024-05-01 14:36:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crafter Addon
+
+
+A simple datapack that makes the smithed crafter compatible with the majority of vanilla crafting
+
---
+++ main.py
@@ -0,0 +1,225 @@
+
+from beet import Context, Function, Recipe, ItemTag, FunctionTag
+from beet.contrib.vanilla import Vanilla
+
+from nbtlib.tag import (
+    Compound,
+    Int,
+    List,
+    String,
+    Float,
+    Double,
+    Byte,
+    Short,
+    Long,
+    IntArray,
+    LongArray,
+)
+from nbtlib import serialize_tag, parse_nbt
+
+from copy import deepcopy
+import json
+
+
+
+MINECRAFT_VERSION = "1.20.6"
+NAMESPACE = "crafter_addon"
+
+
+
+def beet_default(ctx: Context):
+    ctx.inject(Vanilla)
+
+    mc_version = ctx.meta.get("target_mc_version", MINECRAFT_VERSION)
+
+    recipes = ctx.inject(Vanilla).releases[mc_version].data.recipes
+    for recipe in recipes:
+        transform_recipes(ctx, recipes[recipe], recipe)
+
+    
+
+
+def get_result_command(result: dict[str,str]):
+    nbt = parse_nbt(json.dumps(result))
+    nbt[String("Slot")] = Byte(16)
+    return f"data modify block ~ ~ ~ Items append value {serialize_tag(nbt)}"
+
+
+
+
+def transform_recipes(ctx: Context, recipe: Recipe, recipe_name: str):
+    match recipe.data["type"]:
+        case "minecraft:crafting_shaped":
+            transform_shaped(ctx, recipe, recipe_name)
+        case "minecraft:crafting_shapeless":
+            transform_shapeless(ctx, recipe, recipe_name)
+    
+
+
+
+
+def transform_shaped(ctx: Context, recipe: Recipe, recipe_name: str):
+    line_count = len(recipe.data["pattern"])
+
+    void_line = List[Compound]([
+        Compound({String("id") : String("minecraft:air"), String("Slot") : Byte(0)}),
+        Compound({String("id") : String("minecraft:air"), String("Slot") : Byte(1)}),
+        Compound({String("id") : String("minecraft:air"), String("Slot") : Byte(2)}),
+    ])
+
+    match line_count:
+        case 1:
+            shaped_recipe = Compound({
+                String("0") : deepcopy(void_line),
+            })
+            void_lines = "\n\tif data storage smithed.crafter:input {recipe:{1:[]}}"
+            void_lines += "\n\tif data storage smithed.crafter:input {recipe:{2:[]}}"
+        case 2:
+            shaped_recipe = Compound({
+                String("0") : deepcopy(void_line),
+                String("1") : deepcopy(void_line),
+            })
+            void_lines = "\n\tif data storage smithed.crafter:input {recipe:{2:[]}}"
+        case 3:
+            shaped_recipe = Compound({
+                String("0") : deepcopy(void_line),
+                String("1") : deepcopy(void_line),
+                String("2") : deepcopy(void_line),
+            })
+            void_lines = ""
+        case _:
+            raise NotImplementedError(f"Recipe with {line_count} lines is not supported")
+    
+    for i, line in enumerate(recipe.data["pattern"]):
+        for j, char in enumerate(line):
+            if char == " ":
+                # it's an empty slot
+                continue
+            item : dict[str,str] | list[dict[str,str]] = recipe.data["key"][char]
+            if isinstance(item, list):
+                # create a new item tag
+                tag_name = f"{NAMESPACE}:recipes/{recipe_name.replace(':','/')}/{i}/{j}"
+                item_list = []
+                for it in item:
+                    if "item" in it:
+                        item_list.append(it["item"])
+                    elif "tag" in it:
+                        item_list.append(f"#{it['tag']}")
+                    else:
+                        raise NotImplementedError("Unknown item format")
+                ctx.data.item_tags[tag_name] = ItemTag({
+                    "values": item_list
+                })
+                item = {"tag": tag_name}
+
+            if "item" in item:
+                item_id = item["item"]
+                shaped_recipe[str(i)][j]["id"] = String(item_id)
+            elif "tag" in item:
+                tag = f'#{item["tag"]}'
+                del shaped_recipe[str(i)][j]["id"]
+                shaped_recipe[str(i)][j]["item_tag"] = List[String]([String(tag)])
+            else:
+                raise NotImplementedError("Unknown item format")
+            
+    
+    result_command = get_result_command(recipe.data["result"])
+
+
+    function_name = f"{NAMESPACE}:shaped"
+    if not function_name in ctx.data.functions:
+        ctx.data.functions[function_name] = Function()
+        smithed_tag = "smithed.crafter:event/recipes"
+        if not smithed_tag in ctx.data.function_tags:
+            ctx.data.function_tags[smithed_tag] = FunctionTag()
+        ctx.data.function_tags[smithed_tag].add(function_name)
+
+    command = f"""
+execute 
+    store result score @s smithed.data
+    if entity @s[scores={{smithed.data=0}}]
+    if data storage smithed.crafter:input recipe{serialize_tag(shaped_recipe)} {void_lines}
+    run {result_command}
+
+"""
+    ctx.data.functions[function_name].append(command)
+
+
+def transform_shapeless(ctx: Context, recipe: Recipe, recipe_name: str):
+    ingredients = recipe.data["ingredients"]
+
+    ingredients_map= []
+
+    for i, ingredient in enumerate(ingredients):
+
+        ingredient_in_map = False
+        index_ingredient = -1
+        for j, item in enumerate(ingredients_map):
+            if item["ingredient"] == ingredient:
+                ingredient_in_map = True
+                index_ingredient = j
+                break
+        if ingredient_in_map:
+            ingredients_map[index_ingredient]["count"] += 1
+        else:
+            ingredients_map.append({
+                "ingredient": ingredient,
+                "count": 1
+            })
+
+    shapeless_recipe = List[Compound]()
+    
+    for i, item in enumerate(ingredients_map):
+        ingredient = item["ingredient"]
+        if isinstance(ingredient, list):
+            # create a new item tag
+            tag_name = f"{NAMESPACE}:recipes/{recipe_name.replace(':','/')}/{i}"
+            item_list = []
+            for it in ingredient:
+                if "item" in it:
+                    item_list.append(it["item"])
+                elif "tag" in it:
+                    item_list.append(f"#{it['tag']}")
+                else:
+                    raise NotImplementedError("Unknown item format")
+            ctx.data.item_tags[tag_name] = ItemTag({
+                "values": item_list
+            })
+            ingredient = {"tag": tag_name}
+        
+        if "item" in ingredient:
+            item_id = ingredient["item"]
+            shapeless_recipe.append(Compound({
+                "id": String(item_id),
+                "count": Int(item["count"]),
+            }))
+        elif "tag" in ingredient:
+            tag = f'#{ingredient["tag"]}'
+            shapeless_recipe.append(Compound({
+                "item_tag": List[String]([String(tag)]),
+                "count": Int(item["count"]),
+            })
+            )
+        else:
+            raise NotImplementedError("Unknown item format")
+
+    result_command = get_result_command(recipe.data["result"])
+
+    function_name = f"{NAMESPACE}:shapeless"
+    if not function_name in ctx.data.functions:
+        ctx.data.functions[function_name] = Function()
+        smithed_tag = "smithed.crafter:event/shapeless_recipes"
+        if not smithed_tag in ctx.data.function_tags:
+            ctx.data.function_tags[smithed_tag] = FunctionTag()
+        ctx.data.function_tags[smithed_tag].add(function_name)
+
+    command = f"""
+execute 
+    store result score @s smithed.data 
+    if entity @s[scores={{smithed.data=0}}] 
+    if score count smithed.data matches {len(ingredients_map)}
+    if data storage smithed.crafter:input {{recipe:{serialize_tag(shapeless_recipe)}}}
+    run {result_command}
+"""
+    
+    ctx.data.functions[function_name].append(command)
